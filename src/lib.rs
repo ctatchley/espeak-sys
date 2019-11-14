a4fe90cd172228dad47328beb397c15cbd51f44b
Sergio Tortosa <sergio@sergio-mini-laptop>
2019-11-14 18:54:09 +0100

    Use espeak-ng library

--- src/lib.rs
+++ src/lib.rs
@@ -150,7 +150,7 @@ impl espeak_VOICE {
 
 pub type t_espeak_callback = extern "C" fn(*mut c_short, c_int, *mut espeak_EVENT) -> c_int;
 
-#[link(name = "espeak")]
+#[link(name = "espeak-ng")]
 extern "C" {
 	pub fn espeak_Initialize(output: espeak_AUDIO_OUTPUT, buflength: c_int, path: *const c_char, options: c_int) -> c_int;
 	pub fn espeak_SetSynthCallback(SynthCallback: t_espeak_callback);
